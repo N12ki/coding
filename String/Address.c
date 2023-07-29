35c35f389a8d6a99654cc739d9e333f548681416
N12ki <140584006+N12ki@users.noreply.github.com>
2023-07-29 16:11:21 +0530

    Add files via upload

---
+++ String/Address.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+	char arr[4]={'a','b','v','l'};
+	printf("%p\n",&arr[0]);
+	printf("%p\n",&arr[1]);
+	printf("%p\n",&arr[2]);
+	printf("%p\n",&arr[3]);
+	return 0;
+}
---
+++ String/input,string.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+/*int main() {
+    char name[3][50];
+    int i;
+    for(i=0;i<3;i++)
+    {
+      printf("Enter your name: ");
+      scanf("%s", name[i]);
+    }
+    for(i=0;i<3;i++)
+    {
+       printf("Hello %s!\n", name[i]);
+    }
+    return 0;
+}*/
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+    printf("How many names do you want to print: ");
+    scanf("%d", &n);
+
+    char** names = (char**)malloc(n * sizeof(char*));
+
+    for (int i = 0; i < n; i++) {
+        names[i] = (char*)malloc(50 * sizeof(char));
+        printf("Enter name %d: ", i + 1);
+        scanf("%49s", names[i]);
+    }
+
+    printf("Names entered:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%s\n", names[i]);
+        free(names[i]);
+    }
+
+    free(names);
+    return 0;
+}
+
